b85c31dfe775224231ae1c3eb32c2109637e4006
Pratham Dalmia <dalmiapratham7@gmail.com>
2025-03-22 20:52:46 +0530

    Added files after making 'Deletion Package'

---
+++ src/Deletion/DeleteProduct.java
@@ -0,0 +1,26 @@
+package Deletion;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DeleteProduct {
+	public void delete(Connection conn,int product_id) {
+		try {
+			CallableStatement stmt = conn.prepareCall("{CALL Delete_Product(?)}");
+			stmt.setInt(1,product_id);
+			
+			
+			stmt.execute();
+			
+			System.out.println("Deleted Product with ID "+product_id);
+			
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
